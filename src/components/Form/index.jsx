a52bbb0c98b6a31fa613bb17e6d13abada86d33b
Ant0yne <antoine.soliv@gmail.com>
2024-03-21 14:55:45 +0100

    onChange-onSubmit-base

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,3 +1,5 @@
+import handleSubmit from "../../assets/js/handleSubmit";
+
 import "./form.css";
 
 const Form = ({
@@ -14,14 +16,30 @@ const Form = ({
 		<>
 			<main>
 				<h1>Create account</h1>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div>
 						<h2>Name</h2>
-						<input type="text" placeholder="Your name..." name="name" />
+						<input
+							type="text"
+							placeholder="Your name..."
+							name="name"
+							value={name}
+							onChange={(e) => {
+								setName(e.target.value);
+							}}
+						/>
 					</div>
 					<div>
 						<h2>Email</h2>
-						<input type="email" placeholder="Your email..." name="email" />
+						<input
+							type="email"
+							placeholder="Your email..."
+							name="email"
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
 					</div>
 					<div>
 						<h2>Password</h2>
@@ -29,6 +47,10 @@ const Form = ({
 							type="password"
 							placeholder="Your password..."
 							name="password"
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
 						/>
 					</div>
 					<div>
@@ -37,6 +59,10 @@ const Form = ({
 							type="password"
 							placeholder="Confirm your password..."
 							name="passwordConfirm"
+							value={passwordConfirm}
+							onChange={(e) => {
+								setPasswordConfirm(e.target.value);
+							}}
 						/>
 					</div>
 					<input type="submit" value="Register" />
